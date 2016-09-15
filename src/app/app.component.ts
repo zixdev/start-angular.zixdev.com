523b868d73cf2853f6beff95b9343776b5d09766
badi ifaoui <badi1wow@gmail.com>
2016-09-15 19:10:30 +0100

    updated: header, footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,7 @@ import {Component} from "@angular/core";
 
 @Component({
     selector: 'app-root',
-    templateUrl: 'app.component.html',
-    styleUrls: ['app.component.scss', './../mdbootstrap/scss/app.scss']
+    templateUrl: 'app.component.html'
 })
 export class AppComponent {
     title = 'app works!';
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -18,4 +18,4 @@ import 'core-js/es6/reflect';
 import 'core-js/es7/reflect';
 import 'zone.js/dist/zone';
 
-// import './mdbootstrap/js/app.js';
+import './mdbootstrap/js/app.js';
