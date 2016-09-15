59b84564c79aad86cdeda15927a4f2aa3748c721
badi ifaoui <badi1wow@gmail.com>
2016-09-15 20:05:01 +0100

    updated: header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
+import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
+import {AppComponent} from "./app.component";
+import {HomeComponent} from "./pages/home/home.component";
+import {HeaderComponent, FooterComponent} from "./components";
+import {RouterModule} from "@angular/router";
+import { HomeBannerComponent } from './components/home-banner/home-banner.component';
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './pages/home/home.component';
-import { HeaderComponent, FooterComponent } from './components';
+const APP_ROUTES = [
+    {path: '', component: HomeComponent}
+];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    HeaderComponent,
-    FooterComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        HeaderComponent,
+        FooterComponent,
+        HomeBannerComponent,
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        RouterModule.forRoot(APP_ROUTES, {useHash: true})
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
