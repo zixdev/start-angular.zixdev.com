e75b2bb0f3dd0eed46dddf06216e991a59ebfda8
badi ifaoui <badi1wow@gmail.com>
2016-09-15 19:14:22 +0100

    code :cleaning

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,7 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent, FooterComponent } from './components';
 
 @NgModule({
   declarations: [
