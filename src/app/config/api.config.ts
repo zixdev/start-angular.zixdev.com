4b747a6013ee9868d16f7a83c964bb41551829f4
badi ifaoui <badi1wow@gmail.com>
2016-09-18 16:43:22 +0100

    updated: config, modules

--- src/app/config/api.config.ts
+++ src/app/config/api.config.ts
@@ -1,4 +1,5 @@
 export const ApiConfig = {
     api_url: 'http://localhost:8000/api/',  // The Api link should end with `/` => 'http://api.example.com/'
-    JWT_TOKEN: 'token',                 // default api token name in user local storage 'token'
+    JWT_TOKEN: 'token',                 // default api token name in user local storage 'token',
+    site_public_code: '525406d96911e9074f9fc2fff82a34cd'
 };
